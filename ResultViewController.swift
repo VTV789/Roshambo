4c10ae48b7047019214bd16ae27d4bfe7f4599dd
Vinh Thanh Vu <vtv@vinh.com>
2016-01-17 23:51:40 -0500

    Create a ResultViewController

---
+++ ResultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  resultViewController.swift
+//  Roshambo
+//
+//  Created by Vinh Vu on 1/17/16.
+//  Copyright © 2016 Vinh. All rights reserved.
+//
+
+import Foundation
---
+++ rock/rock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rock
+//
+//  Created by Vinh Vu on 1/17/16.
+//  Copyright © 2016 Vinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
